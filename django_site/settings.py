eaa58cf9ad578bab6223e00add207be8ceac10ad
Thomas Leonard <thomas.christophe.leonard@gmail.com>
2017-03-03 16:49:07 +0100

    add latest_magnet app to the site

--- django_site/settings.py
+++ django_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'latest_magnet.apps.LatestMagnetConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
