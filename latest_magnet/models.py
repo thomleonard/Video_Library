dc1a2f8d6d5662132c8aed12d26bc7f34173a992
Thomas Leonard <thomas.christophe.leonard@gmail.com>
2017-03-03 16:49:53 +0100

    create the model for a tv show

--- latest_magnet/models.py
+++ latest_magnet/models.py
@@ -1,5 +1,8 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class TVShow(models.Model):
+    title = models.CharField(max_length=100)
+    current_season = models.PositiveIntegerField()
+    current_episode = models.PositiveIntegerField()
+    is_next_episode_available = models.BooleanField()
+    next_episode_date = models.DateField()
