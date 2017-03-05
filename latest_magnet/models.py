48f474bc174d143609e7b5dbd08b30a72b7f33c8
Thomas Leonard <thomas.christophe.leonard@gmail.com>
2017-03-05 18:46:10 +0100

    setup tvshow model

--- latest_magnet/models.py
+++ latest_magnet/models.py
@@ -1,8 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
 class TVShow(models.Model):
-    title = models.CharField(max_length=100)
+    """
+    TV show model:
+
+    """
+    title = models.CharField(max_length=100, unique=True)
     current_season = models.PositiveIntegerField()
-    current_episode = models.PositiveIntegerField()
+    last_seen_episode = models.PositiveIntegerField()
     is_next_episode_available = models.BooleanField()
-    next_episode_date = models.DateField()
+    update_date = models.DateTimeField(default=None)
+    next_episode_date = models.DateField(default=None, null=True)
+
+    def add_new(self, title):
+        """
+        add a new TV show to the database.
+        """
+        print 'here'
+        self.title = title
+        self.current_season = 1
+        self.last_seen_episode = 1
+        self.is_next_episode_available = True
+        self.update_date = timezone.now()
+        print 'title set'
+        self.save()
