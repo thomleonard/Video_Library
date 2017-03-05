5964930309548071e1c2d16fd079cbed8e81e1d5
Thomas Leonard <thomas.christophe.leonard@gmail.com>
2017-03-05 19:04:41 +0100

    addd a clean database button

--- latest_magnet/views.py
+++ latest_magnet/views.py
@@ -9,21 +9,30 @@ def search(request):
     """
     if request.method == 'POST':
         # POST request
-        form = SearchName(request.POST)
-        if form.is_valid():
-            title = form['name'].value()
-            # if the TV show is not already in the database
-            # add it
-            if not TVShow.objects.filter(title=title).exists():
-                tvshow = TVShow()
-                tvshow.add_new(title)
+        if '_clean' in request.POST:
+            # clean the database
+            for tvshow in TVShow.objects.all():
+                tvshow.delete()
+        else:
+            form = SearchName(request.POST)
+            if form.is_valid():
+                title = form['name'].value()
+                # if the TV show is not already in the database
+                # add it
+                if not TVShow.objects.filter(title=title).exists():
+                    tvshow = TVShow()
+                    tvshow.add_new(title)
     
-    else: # GET
-        form = SearchName()
 
-    # print 'len', len(TVShow.objects.all())
+    else:
+        pass
+    form = SearchName()
+
+    print 'len', len(TVShow.objects.all())
     # print 'get', TVShow.objects.get(title='yo')
 
     # instance = SomeModel.objects.get(id=id)
     # instance.delete()
+    for tvshow in TVShow.objects.order_by('-update_date'):
+        print tvshow.update_date, tvshow.title
     return render(request, 'latest_magnet/search.html', {'form': form})
