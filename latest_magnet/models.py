d493acee1eb11acd7c024b74cc478bd2447320e6
Thomas Leonard <thomas.christophe.leonard@gmail.com>
2017-03-06 15:48:10 +0100

    add url attribute to TVShow model

--- latest_magnet/models.py
+++ latest_magnet/models.py
@@ -7,6 +7,7 @@ class TVShow(models.Model):
 
     """
     title = models.CharField(max_length=100, unique=True)
+    url = models.CharField(max_length=100, unique=True)
     current_season = models.PositiveIntegerField()
     last_seen_episode = models.PositiveIntegerField()
     is_next_episode_available = models.BooleanField()
@@ -17,9 +18,16 @@ class TVShow(models.Model):
         """
         add a new TV show to the database.
         """
-        self.title = title
+        self.title = title.lower() # title must be only lower case
+        self.url = title_to_url(title)
         self.current_season = 1
         self.last_seen_episode = 1
         self.is_next_episode_available = True
         self.update_date = timezone.now()
         self.save()
+
+def title_to_url(title):
+    url = title.replace(' ', '.') # replace spaces by dots
+    url = url.replace("'", "") # remove '
+    url = url.lower() # make sure it is only lower case
+    return url
