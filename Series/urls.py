672201bee25889eee6c1014b6f0aa289f2f51364
Thomas Leonard <thomas.christophe.leonard@gmail.com>
2017-04-02 12:07:04 +0100

    some comments on urls

--- Series/urls.py
+++ Series/urls.py
@@ -5,8 +5,14 @@ from django.conf.urls.static import static
 from . import views
 
 app_name = 'Series'
+
 urlpatterns = [
+
+    # TV Shows library and search page
     url(r'^$', views.library, name='library'),
+
+    # TV Show page
     url(r'^tvshow_(?P<tvshow_pk>\d+)/$',
     	views.tvshow_page, name='tvshow_page'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
